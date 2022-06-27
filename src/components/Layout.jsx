621d6d09c82a9eff94e13d328b7dabb33dae0e50
AndryPribadi <pribadiandry@gmail.com>
2022-06-27 12:34:01 +0800

    Struktur e-commerce app

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Layout = (props) => {
+  return (
+    <div className="w-full h-screen flex flex-col overflow-auto">
+      <div className="h-full w-full bg-white dark:bg-black">
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
