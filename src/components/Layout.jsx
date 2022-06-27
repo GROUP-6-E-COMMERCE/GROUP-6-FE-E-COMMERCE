ccb4480ea387f679be19d46273af93bf3020e106
AndryPribadi <pribadiandry@gmail.com>
2022-06-27 21:08:13 +0800

    Slicing page register

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Layout = (props) => {
   return (
     <div className="w-full h-screen flex flex-col overflow-auto">
-      <div className="h-full w-full bg-white dark:bg-black">
+      <div className="h-full w-full bg-neutral-200 dark:bg-black">
         {props.children}
       </div>
     </div>
